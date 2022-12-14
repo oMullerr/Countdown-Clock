21eb6573c9f76ba2789884a72b4ffc9f31aee01f
oMullerr <leindorfmuller@gmail.com>
2022-12-13 21:42:35 -0300

    Countdown Clock

---
+++ main.js
@@ -0,0 +1,23 @@
+let deadline = new Date("mar 09, 2023 15:00:00").getTime();
+  
+let x = setInterval(function() {
+  
+const now = new Date().getTime();
+const t = deadline - now;
+const days = Math.floor(t / (1000 * 60 * 60 * 24));
+const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+document.getElementById("day").innerHTML = days ;
+document.getElementById("hour").innerHTML = hours;
+document.getElementById("minute").innerHTML = minutes; 
+document.getElementById("second").innerHTML = seconds; 
+
+if (t < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "TIME UP";
+    document.getElementById("day").innerHTML ='0';document.getElementById("hour").innerHTML ='0';
+    document.getElementById("minute").innerHTML ='0' ; 
+    document.getElementById("second").innerHTML = '0'; }
+}, 1000);
